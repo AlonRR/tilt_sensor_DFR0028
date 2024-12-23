43ed1227dffc5e5a5f4f5c23dbd54869a8b91bf3
Alon A. Rabinowitz <alonrab@outlook.com>
2024-12-23 20:26:30 +0200

    add tilt sensor functionality to main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
+#define TILT_PIN 26
+
 void setup()
 {
     Serial.begin(115200);
+    pinMode(TILT_PIN, INPUT);
 }
 
 void loop()
 {
+    Serial.println(digitalRead(TILT_PIN));
+    delay(50);
 }
